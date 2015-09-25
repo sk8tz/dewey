5ee49f0c65706d9aa25a3d1d48c84bf2ff444a1c
Austin Winstanley <austin@axialcommerce.com>
2015-09-24 21:04:35 -0500

    added to temporal

---
+++ src/Dewey.Net.Mvc/Temporal/TimeModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using Dewey.Net.Temporal;
+using System.Web.Mvc;
+
+namespace Dewey.Net.Mvc.Temporal
+{
+    public class TimeModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var key = bindingContext.ModelName;
+
+            var val = bindingContext.ValueProvider.GetValue(key);
+
+            if (val != null) {
+                var s = val.AttemptedValue as string;
+
+                if (s != null) {
+                    return new Time(s);
+                }
+            }
+            return null;
+        }
+    }
+}
