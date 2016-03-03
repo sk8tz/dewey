95dfd83f1b48927c2a3be66330e8fe8eb1d9b9c5
Austin Winstanley <austin@axialcommerce.com>
2016-03-02 18:21:41 -0600

    Update IEnumerableExtensions.cs

--- src/Dewey/Types/IEnumerableExtensions.cs
+++ src/Dewey/Types/IEnumerableExtensions.cs
@@ -14,5 +14,12 @@ namespace Dewey.Types
                 action(item);
             }
         }
+        
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+
+            return source.Where(element => seenKeys.Add(keySelector(element)));
+        }
     }
 }
