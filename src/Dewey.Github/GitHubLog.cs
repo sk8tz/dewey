465bba1b91bf494afd60b42b8f728a71b4a0db38
Austin Winstanley <austin@axialcommerce.com>
2016-03-16 12:35:31 -0500

    added Dewey.Github

---
+++ src/Dewey.Github/GitHubLog.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dewey.GitHub
+{
+    public class GitHubLog
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
